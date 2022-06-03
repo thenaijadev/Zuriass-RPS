c3c76f964772387e2ad3628171eb743c48cb1505
thenaijadev <bob@>
2022-06-03 16:37:53 -0700

    final commit

--- main.py
+++ main.py
@@ -1,32 +1,74 @@
-def read_file_content(filename):
-    # [assignment] Add your code here
-    with open(filename) as f:
-        lines = f.readlines()
-        words = ""
-        for line in lines:
-            words = words+line
-        return words
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-text = read_file_content("story.txt")
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 
-text = text.lower()
-print(text)
+#Write your code below this line 👇
+options = [rock,paper,scissors]
+number = random.randint(1,3)
+number-=1
 
+computer_choice = options[number]
+isValid = True
 
-def count_words():
-    counts = dict()
-    words = read_file_content("story.txt")
-    words = words.split()
-    for word in words:
+def play():
+    choice = input("Rock Paper Scissors ")
+    choice = choice.lower()
+    if choice == "paper" and computer_choice == rock:
+        choice = paper
+        print(f"Computer: {computer_choice}\n You: {choice} You win.")
+    elif choice == "scissors" and computer_choice == paper:
+        choice = scissors
+        print(f"Computer: {computer_choice}\n You: {choice} You loose.")
+    elif choice == "rock" and computer_choice == paper:
+        choice = rock
+        print(f"Computer: {computer_choice}\n You: {choice} You loose.")
+    elif choice == "rock" and computer_choice == scissors:
+        choice = rock
+        print(f"Computer: {computer_choice}\n You: {choice} You win.")
+    elif choice == "paper" and computer_choice == scissors:
+        choice = paper
+        print(f"Computer: {computer_choice}\n You: {choice} You loose.")
+    elif choice == "scissors" and computer_choice == rock:
+        choice = scissors
+        print(f"Computer: {computer_choice}\n You: {choice} You loose.")
+    elif choice == "scissors" and computer_choice == scissors:
+        choice = scissors
+        print(f"Computer: {computer_choice}\n You: {choice} Draw.")
+        play()
+    elif choice == "rock" and computer_choice == rock:
+        choice = rock
+        print(f"Computer: {computer_choice}\n You: {choice} Draw.")
+        play()
+    elif choice == "paper" and computer_choice == paper:
+        choice = paper
+        print(f"Computer: {computer_choice}\n You: {choice} Draw.")
+        play()
+    else:
+        print("Invalid input,Choose again.")
+        play()
 
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
-
-    return counts
-
-
-word_dict = count_words()
-print(word_dict)
+play()
